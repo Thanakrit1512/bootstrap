3fd24f51c4e673c703f9e3775a22b96dd0d9ae8a
lu69x <saprasit.tn@gmail.com>
2019-04-16 02:53:28 +0700

    add status indicator

--- html/script/node-tracker-script.js
+++ html/script/node-tracker-script.js
@@ -8,58 +8,10 @@ var ld2 = document.getElementById('ld2')
 var nw1 = document.getElementById('nw1')
 var nw2 = document.getElementById('nw2')
 
-
-
 updateState()
 window.setInterval(updateState, 1500);
 
 function updateState(){
-
-    $.ajax({
-        url: "/getnode/0001",
-        type: 'GET',
-        dataType:'json',
-        success: function(res){
-            dt1.innerHTML = res[0].date
-            if(res[0].sw_status == 'T'){
-                sw1.innerHTML = 'On'
-                sw1.style.backgroundColor = "#2add4e"
-            }else{
-                sw1.innerHTML = 'Off'
-                sw1.style.backgroundColor = "#dd2a2a"
-            }
-            if(res[0].ld_status == 'T'){
-                ld1.innerHTML = 'On'
-                ld1.style.backgroundColor = "#2add4e"
-            }else{
-                ld1.innerHTML = 'Off'
-                ld1.style.backgroundColor = "#dd2a2a"
-            }
-        }
-    })
-    $.ajax({
-        url: "/getnode/0002",
-        type: 'GET',
-        dataType:'json',
-        success: function(res){
-            dt2.innerHTML = res[0].date
-            if(res[0].sw_status == 'T'){
-                sw2.innerHTML = 'On'
-                sw2.style.backgroundColor = "#2add4e"
-            }else{
-                sw2.innerHTML = 'Off'
-                sw2.style.backgroundColor = "#dd2a2a"
-            }
-            if(res[0].ld_status == 'T'){
-                ld2.innerHTML = 'On'
-                ld2.style.backgroundColor = "#2add4e"
-            }else{
-                ld2.innerHTML = 'Off'
-                ld2.style.backgroundColor = "#dd2a2a"
-            }
-        }
-    })
-
     if(d.getTime() - new Date(dt1.innerHTML).getTime() <= 35000){
         nw1.innerHTML = 'Online'
         nw1.style.backgroundColor='green';
@@ -81,14 +33,51 @@ function updateState(){
         nw2.style.backgroundColor='red';
     }
 
-    
+    $.ajax({
+        url: "getnode/0001",
+        type: 'GET',
+        dataType:'text',
+        success: function(res){
+            console.log(res)
+            dt1.innerHTML = d
+            if(res[0].ld_state == 'T'){
+                ld1.innerHTML = 'On'
+                ld1.style.backgroundColor = "#2add4e"
+            }else{
+                ld1.innerHTML = 'Off'
+                ld1.style.backgroundColor = "#dd2a2a"
+            }
+        }
+    })
+    $.ajax({
+        url: "getnode/0002",
+        type: 'GET',
+        dataType:'text',
+        success: function(res){
+            console.log(res)
+            dt2.innerHTML = d
+            if(res[0].ld_state == 'T'){
+                ld2.innerHTML = 'On'
+                ld2.style.backgroundColor = "#2add4e"
+            }else{
+                ld2.innerHTML = 'Off'
+                ld2.style.backgroundColor = "#dd2a2a"
+            }
+        }
+    })
 }
 
-
-
+    
 
 
 function turnOff(node){
+    if(node == '0001'){
+        sw1.innerHTML = 'On'
+        sw1.style.backgroundColor = "#2add4e"
+    }else if(node == '0001'){
+        sw2.innerHTML = 'On'
+        sw2.style.backgroundColor = "#2add4e"
+    }
     $.ajax({
         url: "/command/" + node + "/00",
         type: 'GET',
@@ -99,6 +88,13 @@ function turnOff(node){
     })
 }
 function turnOn(node){
+    if(node == '0002'){
+        sw1.innerHTML = 'Off'
+        sw1.style.backgroundColor = "#2add4e"
+    }else if(node == '0002'){
+        sw2.innerHTML = 'Off'
+        sw2.style.backgroundColor = "#2add4e"
+    }
     $.ajax({
         url: "/command/" + node + "/01",
         type: 'GET',
